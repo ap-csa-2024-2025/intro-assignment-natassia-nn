5b5c31e5e2a53bc26a6459ffaf79152608c1b78a
natassia-nn <natassianguyen@gmail.com>
2024-09-03 10:05:27 -0700

    new lesson changes

---
+++ Printing.java
@@ -0,0 +1,15 @@
+public class Printing
+{
+    public static void main(String[] args)
+    {
+      // int, double, boolean, String
+      int myAge = 999; // whole number; integers
+      double price = 4.99; // decimals
+
+      boolean isGood; // variable initialization
+      isGood = false;   
+
+      String name = "Natassia Nguyen"; // set name to "Natassia Nguyen"
+      System.out.print("Hello my name is " + name);
+    }
+}
